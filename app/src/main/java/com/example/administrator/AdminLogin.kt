a81fb3e1670c63e49893500f596698c6a90953b7
matsudafumito <230372b@ugs.kochi-tech.ac.jp>
2022-01-16 19:21:56 +0900

    AdminLogin.ktを作成

--- app/src/main/java/com/example/administrator/AdminLogin.kt
+++ app/src/main/java/com/example/administrator/AdminLogin.kt
@@ -1,17 +1,34 @@
 package com.example.administrator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.net.URI
+import org.json.JSONObject
 
 class AdminLogin : AppCompatActivity() {
+    companion object{
+        const val loginReqId: Int = 1
+    }
+
+    private val uri = WsClient.serverRemote
+    private var client = LoginWsClient(this, uri)
+
+    override fun onRestart() {
+        super.onRestart()
+        client = LoginWsClient(this, uri)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_login)
 
+        client.connect()
         //view
         val textBoxUserName : EditText = findViewById(R.id.textBoxUserName)
 
@@ -19,8 +36,22 @@ class AdminLogin : AppCompatActivity() {
 
         val buttonLogin : Button = findViewById(R.id.buttonLogin)
         buttonLogin.setOnClickListener {
-            val intent = Intent(this, Administrator::class.java)
-            startActivity(intent)
+            val loginParams = JSONObject()
+            val userName: String = textBoxUserName.text.toString()
+            val password: String = textBoxPassword.text.toString()
+            loginParams.put("user_name", userName)
+            loginParams.put("password", password)
+            loginParams.put("role", "admin")
+            val loginRequest = client.createJsonrpcReq("login", loginReqId, loginParams)
+
+            try{
+                if(client.isClosed) {
+                    client.reconnect()
+                }
+                client.send(loginRequest.toString())
+            } catch (ex: Exception){
+                Log.i(javaClass.simpleName, "send failed $ex")
+            }
         }
 
         val buttonAccount : Button = findViewById(R.id.buttonAccount)
@@ -31,4 +62,40 @@ class AdminLogin : AppCompatActivity() {
 
     }
 
+}
+
+class LoginWsClient(private val activity: Activity, uri: URI) : WsClient(uri){
+    override fun onMessage(message: String?) {
+        super.onMessage(message)
+        Log.i(javaClass.simpleName, "msg arrived")
+        Log.i(javaClass.simpleName, "$message")
+        val wholeMsg = JSONObject("$message")
+        val resId: Int = wholeMsg.getInt("id")
+        val result: JSONObject = wholeMsg.getJSONObject("result")
+        val status: String = result.getString("status")
+        //if message is about login
+        if(resId == AdminLogin.loginReqId){
+            if(status == "success"){
+                val token: String = result.getString("token")
+                val expire: String = result.getString("expire")
+                Log.i(javaClass.simpleName, "login success")
+                Log.i(javaClass.simpleName, "token: $token")
+                Log.i(javaClass.simpleName, "expires in $expire")
+
+                this.close(NORMAL_CLOSURE)
+                activity.runOnUiThread{
+                    val intent = Intent(activity, Administrator::class.java)
+                    intent.putExtra("token", token)
+                    intent.putExtra("expire", expire)
+                    activity.startActivity(intent)
+                }
+
+            }else if(status == "error"){
+                activity.runOnUiThread{
+                    val reason: String = result.getString("reason")
+                    Log.i(javaClass.simpleName, "login failed with reason $reason")
+                }
+            }
+        }
+    }
 }
