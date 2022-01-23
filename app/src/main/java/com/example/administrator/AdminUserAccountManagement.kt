7016e647c98b5a2fdbf6d89edc316c6cbf3826c3
matsudafumito <230372b@ugs.kochi-tech.ac.jp>
2022-01-23 16:44:32 +0900

    検索したアカウント情報を表示させる

--- app/src/main/java/com/example/administrator/AdminUserAccountManagement.kt
+++ app/src/main/java/com/example/administrator/AdminUserAccountManagement.kt
@@ -1,16 +1,10 @@
 package com.example.administrator
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import org.json.JSONObject
-import java.net.URI
 
 class AdminUserAccountManagement : AppCompatActivity() {
     companion object{
@@ -18,13 +12,14 @@ class AdminUserAccountManagement : AppCompatActivity() {
     }
 
     private val uri = WsClient.serverRemote
-    private var client = GetUserInfo(this, uri)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_user_account_management)
-        client.connect()
         //view
+
+
+
         val buttonHome : Button = findViewById(R.id.buttonHome)
         buttonHome.setOnClickListener {
             val intent = Intent(this,Administrator::class.java)
@@ -34,61 +29,30 @@ class AdminUserAccountManagement : AppCompatActivity() {
         val Ban : Button = findViewById(R.id.Ban)
 
     }
+    override fun onResume() {
+        super.onResume()
+        val currentUserId = intent.getIntExtra("userId", 0)
+        val currentUserName = intent.getStringExtra("userName")
+        val currentBirthday = intent.getStringExtra("birthday")
+        val currentGender = intent.getStringExtra("gender")
+        val currentEmail = intent.getStringExtra("emailAddr")
+        val currentAddress = intent.getStringExtra("address")
+        val currentCancel = intent.getStringExtra("num_vicious_cancels")
 
-}
-class GetUserInfo(private val activity: Activity, uri: URI) : WsClient(uri){
-    var userId: Int = -1
-    var userName = ""
-    var birthday = ""
-    var gender = ""
-    var emailAddr = ""
-    var address = ""
-    var numViciousCancels = ""
-    var isReceived = false
+        val etxtUserName: EditText = findViewById(R.id.textBoxUserName)
+        val etxtUserBirthday: EditText = findViewById(R.id.textBoxUserBirthday)
+        val etxtUserGender: EditText = findViewById(R.id.textBoxUserGender)
+        val etxtUserEmail: EditText = findViewById(R.id.textBoxUserEmail)
+        val etxtUserAddress: EditText = findViewById(R.id.textBoxUserAddress)
+        val etxtUserCancel: EditText = findViewById(R.id.txtNumViciousCancels)
 
-    private val errorDisplay: TextView by lazy { activity.findViewById(R.id.errorDisplay) }
-    private val txtUserName: TextView by lazy { activity.findViewById(R.id.textBoxUserName) }
-    private val txtBirthDay: TextView by lazy { activity.findViewById(R.id.textBoxUserBirthday) }
-    private val txtGender: TextView by lazy { activity.findViewById(R.id.textBoxUserGender) }
-    private val txtEmail: TextView by lazy { activity.findViewById(R.id.textBoxUserEmail) }
-    private val txtAddress: TextView by lazy { activity.findViewById(R.id.textBoxUserAddress) }
-    private val txtNumViciousCancels: TextView by lazy { activity.findViewById(R.id.txtNumViciousCancels) }
+        etxtUserName.setText(currentUserName)
+        etxtUserBirthday.setText(currentBirthday)
+        etxtUserGender.setText(currentGender)
+        etxtUserEmail.setText(currentEmail)
+        etxtUserAddress.setText(currentAddress)
+        etxtUserCancel.setText(currentCancel)
 
-    override fun onMessage(message: String?) {
-        super.onMessage(message)
-        Log.i(javaClass.simpleName, "msg arrived")
-        Log.i(javaClass.simpleName, "$message")
-
-        val wholeMsg = JSONObject("$message")
-        val resId: Int = wholeMsg.getInt("id")
-        val result: JSONObject = wholeMsg.getJSONObject("result")
-        val status: String = result.getString("status")
-
-        if(resId == AdminUserAccountSearch.getUserInfoId){
-            this.isReceived = true
-            if(status == "success"){
-                this.userId = result.getInt("user_id")
-                this.userName = result.getString("user_name")
-                this.birthday = result.getString("birthday")
-                this.gender = result.getString("gender")
-                this.emailAddr = result.getString("email_addr")
-                this.address = result.getString("address")
-                this.numViciousCancels = result.getString("num_vicious_cancels")
-
-                activity.runOnUiThread{
-                    txtUserName.text = this.userName
-                    txtBirthDay.text = this.birthday
-                    txtGender.text = this.gender
-                    txtEmail.text = this.emailAddr
-                    txtAddress.text = this.address
-                    txtNumViciousCancels.text = this.numViciousCancels
-                }
-            }else if(status == "error"){
-                activity.runOnUiThread{
-                    errorDisplay.text = "アカウント情報を取得できません"
-                    errorDisplay.visibility = View.INVISIBLE
-                }
-            }
-        }
     }
+
 }
