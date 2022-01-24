11184b0a1446fee67594207a23038f9d717fa43f
FujimotoShinsei <230367w@ugs.kochi-tech.ac.jp>
2022-01-24 20:04:48 +0900

    店舗検索、管理機能を追加。（idの変更必要かも。）

--- app/src/main/java/com/example/administrator/AdminRestaurantAccountManagement.kt
+++ app/src/main/java/com/example/administrator/AdminRestaurantAccountManagement.kt
@@ -4,22 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 
 class AdminRestaurantAccountManagement : AppCompatActivity() {
+    companion object{
+        const val getRestaurantInfoId: Int = 5
+    }
+
+    private val uri = WsClient.serverRemote
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_restaurant_account_management)
-
         //view
+
+
+
         val buttonHome : Button = findViewById(R.id.buttonHome)
         buttonHome.setOnClickListener {
             val intent = Intent(this,Administrator::class.java)
             startActivity(intent)
         }
-
-        val Ban : Button = findViewById(R.id.Ban)
-
-        val multiAutoCompleteTextView : TextView = findViewById(R.id.multiAutoCompleteTextView)
     }
+    override fun onResume() {
+        super.onResume()
+        val Ban : Button = findViewById(R.id.Ban)
+        val currentRestaurantName = intent.getStringExtra("restaurantName")
+        val currentTimeOpen = intent.getStringExtra("timeOpen")
+        val currentTimeClose = intent.getStringExtra("timeClose")
+        val currentHolidays = intent.getStringExtra("holidays")
+        val currentRestaurantEmail = intent.getStringExtra("emailAddr")
+        val currentRestaurantAddress = intent.getStringExtra("address")
+
+        val etxtRestaurantName: EditText = findViewById(R.id.textBoxRestaurantName)
+        val etxtTimeOpen: EditText = findViewById(R.id.textBoxTimeOpen)
+        val etxtTimeClose: EditText = findViewById(R.id.textBoxTimeClose)
+        val etxtHolidays: EditText = findViewById(R.id.textBoxHolidays)
+        val etxtRestaurantEmail: EditText = findViewById(R.id.textBoxRestaurantEmail)
+        val etxtRestaurantAddress: EditText = findViewById(R.id.textBoxRestaurantAddress)
+
+        etxtRestaurantName.setText(currentRestaurantName)
+        etxtTimeOpen.setText(currentTimeOpen)
+        etxtTimeClose.setText(currentTimeClose)
+        etxtHolidays.setText(currentHolidays)
+        etxtRestaurantEmail.setText(currentRestaurantEmail)
+        etxtRestaurantAddress.setText(currentRestaurantAddress)
+
+        Ban.setOnClickListener {
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/administrator/AdminRestaurantAccountSearch.kt
+++ app/src/main/java/com/example/administrator/AdminRestaurantAccountSearch.kt
@@ -1,19 +1,110 @@
 package com.example.administrator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.SearchView
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URI
+import java.util.*
+import kotlin.concurrent.schedule
 
 class AdminRestaurantAccountSearch : AppCompatActivity() {
+
+    companion object{
+        const val getRestaurantInfoId: Int = 5
+        var token = ""
+    }
+
+    private val uri = WsClient.serverRemote
+    private var client = GetUserInfoWsClient(this, uri)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_restaurant_account_search)
+        client.connect()
 
+        //view
         val buttonHome : Button = findViewById(R.id.buttonHome)
         buttonHome.setOnClickListener {
-            val intent = Intent(this,Administrator::class.java)
+            val intent = Intent(this,AdminRegisterAccount::class.java)
             startActivity(intent)
+        }
+    }
+    //change
+    override fun onResume() {
+        super.onResume()
+        //val errorDisplay: TextView = findViewById(R.id.errorDisplay)
+        val buttonSearch: Button = findViewById(R.id.buttonSearch)
+        val searchtext : SearchView = findViewById(R.id.searchtext)
+
+        val exToken = intent.getStringExtra("token")
+        token = exToken!!
+        Log.i(javaClass.simpleName, "token by getStringExtra: $token")
+
+
+        buttonSearch.setOnClickListener {
+            val searchRestaurant: String = searchtext.query.toString()
+            val getInfoParams = JSONObject()
+            getInfoParams.put("searchBy", "restaurant_name")
+            getInfoParams.put("restaurant_name", searchRestaurant)
+            getInfoParams.put("token", token)
+            val getInfoRequest = client.createJsonrpcReq("getInfo/restaurant/basic", getRestaurantInfoId, getInfoParams)
+            try {
+                if (client.isClosed) {
+                    client.reconnect()
+                }
+                client.send(getInfoRequest.toString())
+                //errorDisplay.text = "情報取得中..."
+                //errorDisplay.visibility = View.VISIBLE
+            } catch (ex: Exception) {
+                Log.i(javaClass.simpleName, "send failed")
+                Log.i(javaClass.simpleName, "$ex")
+            }
+        }
+    }
+}
+
+class GetRestaurantInfoWsClient(private val activity: Activity, uri: URI) : WsClient(uri){
+
+
+    override fun onMessage(message: String?) {
+        super.onMessage(message)
+        Log.i(javaClass.simpleName, "msg arrived")
+        Log.i(javaClass.simpleName, "$message")
+
+        val wholeMsg = JSONObject("$message")
+        val resId: Int = wholeMsg.getInt("id")
+        val result: JSONObject = wholeMsg.getJSONObject("result")
+        val status: String = result.getString("status")
+
+        if(resId == AdminRestaurantAccountSearch.getRestaurantInfoId){
+            if(status == "success"){
+                val intent = Intent(activity, AdminUserAccountManagement::class.java) //画面遷移
+                intent.putExtra("restaurantId", result.getInt("restaurant_id"))
+                intent.putExtra("restaurantName", result.getString("restaurant_name"))
+                intent.putExtra("timeOpen", result.getString("time_open"))
+                intent.putExtra("timeClose", result.getString("time_close"))
+                intent.putExtra("holidaysJson", result.getString("holidays_json"))
+                intent.putExtra("emailAddr", result.getString("email_addr"))
+                intent.putExtra("address" , result.getString("address"))
+                intent.putExtra("token", AdminUserAccountSearch.token)
+                activity.startActivity(intent)
+                this.close(WsClient.NORMAL_CLOSURE)
+
+            }else if(status == "error"){
+                Log.i(javaClass.simpleName, "no user matched")
+                activity.runOnUiThread{
+                    //errorDisplay.text = "アカウント情報を取得できません"
+                    //errorDisplay.visibility = View.INVISIBLE
+                }
+            }
         }
     }
 }
