cf5e0b4f4174d24f9b5a1a62691c62ed7217c3ef
FujimotoShinsei <230367w@ugs.kochi-tech.ac.jp>
2022-01-25 23:48:28 +0900

    Ban機能追加

--- app/src/main/java/com/example/administrator/AdminRestaurantAccountManagement.kt
+++ app/src/main/java/com/example/administrator/AdminRestaurantAccountManagement.kt
@@ -1,10 +1,17 @@
 package com.example.administrator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URI
 
 class AdminRestaurantAccountManagement : AppCompatActivity() {
     companion object{
@@ -12,6 +19,7 @@ class AdminRestaurantAccountManagement : AppCompatActivity() {
     }
 
     private val uri = WsClient.serverRemote
+    private val client = RestaurantResignWsClient(this, uri)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +37,16 @@ class AdminRestaurantAccountManagement : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         val Ban : Button = findViewById(R.id.Ban)
+
         val currentRestaurantName = intent.getStringExtra("restaurantName")
         val currentTimeOpen = intent.getStringExtra("timeOpen")
         val currentTimeClose = intent.getStringExtra("timeClose")
         val currentHolidays = intent.getStringExtra("holidays")
         val currentRestaurantEmail = intent.getStringExtra("emailAddr")
         val currentRestaurantAddress = intent.getStringExtra("address")
+
+        val token = Administrator.globalToken
+        var restaurantName = Administrator.globalRestaurantName
 
         val etxtRestaurantName: EditText = findViewById(R.id.textBoxRestaurantName)
         val etxtTimeOpen: EditText = findViewById(R.id.textBoxTimeOpen)
@@ -51,9 +63,62 @@ class AdminRestaurantAccountManagement : AppCompatActivity() {
         etxtRestaurantAddress.setText(currentRestaurantAddress)
 
         Ban.setOnClickListener {
+            val resignParams = JSONObject()
+            resignParams.put("token", token)
+            resignParams.put("account_role", "restaurant")
+            resignParams.put("account_id", currentRestaurantId)
+            val resignReq = client.createJsonrpcReq("resign/forced",
+                AdminRestaurantAccountManagement.restaurantResignReqId, resignParams)
 
+            try {
+                if (client.isClosed) {
+                    client.reconnect()
+                }
+                client.send(resignReq.toString())
+            } catch (ex: Exception) {
+                Log.i(javaClass.simpleName, "send failed")
+                Log.i(javaClass.simpleName, "$ex")
+                //errorDisplay.text = "インターネットに接続されていません"
+                //errorDisplay.visibility = View.VISIBLE
+            }
         }
 
     }
 
+}
+class RestaurantResignWsClient(private val activity: Activity, uri: URI) : WsClient(uri){
+
+    private val errorDisplay: TextView by lazy {
+        activity.findViewById(R.id.errorDisplay)
+    }
+
+    override fun onMessage(message: String?) {
+        super.onMessage(message)
+        Log.i(javaClass.simpleName, "msg arrived")
+        Log.i(javaClass.simpleName, "$message")
+
+        val wholeMsg = JSONObject("$message")
+        val resId: Int = wholeMsg.getInt("id")
+        val result: JSONObject = wholeMsg.getJSONObject("result")
+        val status: String = result.getString("status")
+
+        if (resId == AdminRestaurantAccountManagement.restaurantResignReqId){
+            if(status == "success"){
+                activity.runOnUiThread {
+                    val intent = Intent(activity, ShowResult::class.java)
+                    intent.putExtra("message", "アカウント削除が完了しました")
+                    intent.putExtra("transitionBtnMessage", "トップへ")
+                    intent.putExtra("isBeforeLogin", false)
+                    activity.startActivity(intent)
+                    this.close(NORMAL_CLOSURE)
+                }
+
+            }else if(status == "error"){
+                activity.runOnUiThread {
+                    errorDisplay.text = "パスワードが間違っています"
+                    errorDisplay.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
 }
